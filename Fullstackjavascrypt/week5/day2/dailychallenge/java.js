901f488516409693a0b00bf7c38dd72e471ab196
gastonkornfeld <gatok37@gmail.com>
2022-04-11 23:45:59 +0300

    week 5 day 2 daily challenge generate the story little changes

--- Fullstackjavascrypt/week5/day2/dailychallenge/java.js
+++ Fullstackjavascrypt/week5/day2/dailychallenge/java.js
@@ -9,27 +9,35 @@ function libIt() {
     let noun = document.getElementById('noun').value;
     let adjective = document.getElementById('adjective').value;
     let person = document.getElementById('person').value;
-    if ((person != '' && adjective != '' && person != '') && (storyDiv.children.length < 3)){
+    let place = document.getElementById('place').value;
+    let verb = document.getElementById('verb').value;
+    if ((
+        person != '' &&
+        adjective != '' &&
+        person != '' &&
+        verb != '' &&
+        place != ''
+        ) && (storyDiv.children.length < 3)) {
         let firstParagraph = [
-            `Once upon a time in a far away land call it Developers intitute`,
-            `We went to the moon to have fun, but the moon turned out to be completely ${adjective}.`,
-            `In the light of the moon, a little ${noun} lay on a leaf.`,
-            `On Thursday, when ${person} woke up, she found she had grown ${noun}`,
-            `There was a boy called ${person} and he almost deserved it.`,
+            `Once upon a time in a far away land call it ${place}`,
+            `We went to the ${place} to have fun, but the moon turned out to be completely ${adjective}.`,
+            `In the light of the moon, a little ${noun} lay on a ${place}.`,
+            `On Thursday, when ${person} woke up, she found she had ${verb} ${noun}`,
+            `There was a boy called ${person} and he almost ${verb}.`,
         ]
         let middleParagraph = [
-            ` ${person} saw a few ${adjective} ${noun} in the distance`,
-            `some ${noun} where growing up and make ${person} to be ${adjective} `,
-            `the end of the ${adjective} ${person} life whas coming to and end, `,
-            `he save the ${noun} again, so ${person} started to think he was a little ${adjective}`,
-            `he was thinking, dont ever tell ${person} anything. If you do, you start missing your ${noun}.`,
+            ` ${person} saw a few ${adjective} ${noun} in the distance of ${place}`,
+            `some ${noun} where growing up in ${place} and make ${person} to be ${adjective} `,
+            `the end of the ${adjective} ${person} life whas coming to and end, so it was tempted to start ${verb}ing`,
+            `he save the ${noun} again ${verb}ing, so ${person} started to think he was a little ${adjective}`,
+            `he was ${verb}ing, dont ever tell ${person} anything. If you do, you start missing your ${noun}.`,
         ]
         let lastParagraph = [
-            `After all, tomorrow is another ${adjective} day.`,
-            `He turned out the light and went into ${person} room. He would be there all night, and he would be there when ${person} waked up in the morning.`,
-            `The creatures outside looked from pig to ${noun}, and from ${noun} to pig, and from pig to ${noun} again; but already it was impossible to say which was which`,
-            `But wherever they go, and whatever happens to ${noun} on the way, in that ${adjective} place on the top of the Forest ${person} and his Bear will always be playing.`,
-            `It is not often that someone comes along who is a true friend and a ${noun} writer. ${person} was both.`,
+            `After all, tomorrow is another ${adjective} day in ${place}.`,
+            `He ${verb} the light and went into ${person} room. He would be there all night, and he would be there when ${person} waked up in the morning.`,
+            `The creatures in ${place} looked from pig to ${noun}, and from ${noun} to pig, and from pig to ${noun} again; but already it was impossible to say which was which`,
+            `But wherever they ${verb}, and whatever happens to ${noun} on the way, in that ${adjective} place on the top of the Forest ${person} and his Bear will always be playing.`,
+            `It is not often that someone ${verb} along who is a true friend and a ${noun} writer. ${person} was both.`,
         ]
         let firstSentence = firstParagraph[Math.floor(Math.random() * 5)];
         let secondSentece = middleParagraph[Math.floor(Math.random() * 5)];
