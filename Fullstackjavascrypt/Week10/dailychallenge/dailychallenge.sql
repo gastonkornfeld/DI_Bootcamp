ec611b9a4ae8c0a11f7c776dd4960d792be7ff98
gastonkornfeld <gatok37@gmail.com>
2022-06-01 17:48:08 +0300

    week 10 day 2 daily challenge sql fetch on databases

---
+++ Fullstackjavascrypt/Week10/dailychallenge/dailychallenge.sql
@@ -0,0 +1,9 @@
+
+select COUNT(actor_id) FROM actorsTable;
+
+INSERT INTO actorsTable VALUES(3, '','','1988-02-02', 4);
+
+-- I tough it will be an error because is not null but it just added an empty value to them
+
+select * from actorsTable;
+
