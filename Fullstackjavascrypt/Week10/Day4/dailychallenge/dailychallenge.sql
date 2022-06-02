b1b4334bc94c2e8962ac3af05c74ccf2348d3a1f
gastonkornfeld <gatok37@gmail.com>
2022-06-02 20:41:25 +0300

    week 10 day 4 correction

--- Fullstackjavascrypt/Week10/Day4/dailychallenge/dailychallenge.sql
+++ Fullstackjavascrypt/Week10/Day4/dailychallenge/dailychallenge.sql
@@ -35,10 +35,6 @@ FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab );
 
 
 
--- Q4. What will be the OUTPUT of the following statement?
-
-    SELECT COUNT(*) 
-    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
 
 
 
