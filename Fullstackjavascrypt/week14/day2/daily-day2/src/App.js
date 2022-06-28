876063285cca7ab0d36149bb97b9e8e828dc8410
gastonkornfeld <gatok37@gmail.com>
2022-06-28 11:37:02 -0400

    week 14 day 2 carrousel on react

---
+++ Fullstackjavascrypt/week14/day2/daily-day2/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Carrousel from './Carrousel';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Carrousel />
+      </header>
+    </div>
+  );
+}
+
+export default App;
